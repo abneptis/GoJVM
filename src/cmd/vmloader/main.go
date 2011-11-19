a35e7dc9e9a212437b22769cd14a4091edad6d07
James D. Nurmi <james@abneptis.com>
2011-11-19 09:53:07 -0800

    Update 'vmloader' bin, now acts as a (Very) poor mans 'java', loding classes through the vm, and calling main()

--- src/cmd/vmloader/main.go
+++ src/cmd/vmloader/main.go
@@ -3,24 +3,42 @@ package main
 import (
 	"gojvm"
 	"log"
+	"flag"
 )
 
+var jrePath = gojvm.DefaultJREPath
+var cpBase  = "."
+func init(){
+	flag.StringVar(&jrePath, "jre", jrePath, "Path to JRE (classes)")
+	flag.StringVar(&cpBase, "cp", cpBase, "(single) ClassPath")
+}
+
+
+
+
 func main(){
+	flag.Parse()
+	if flag.NArg() != 1 {
+		/// TODO: brainfart - golang argv[0]?
+		log.Fatalf("Expected: %s 'class-with-main'", "vmloader")
+	}
 	print("Initializing VM\n")
- 	ctx, err := gojvm.InitializeJVM(0,[]string{".","/usr/lib/jvm/java-6-sun/jre/lib"})
+ 	ctx, err := gojvm.InitializeJVM(0,[]string{cpBase,jrePath})
  	if err != nil {
 		log.Fatalf("err == %s", err)
 	}
+	klass := flag.Arg(0)
 	log.Printf("Ctx: %+v", ctx)
-	inst, err := ctx.Env.NewStringObject("hello larry")
+	inst, err := ctx.Env.GetClassStr(klass)
+
  	if err != nil {
-		log.Fatalf("err == %s", err)
+		log.Fatalf("Couldn't find class %s", err)
 	}
-	log.Printf("Instance: %+v", inst)
-	i, err := inst.CallInt(false, "length")
-	log.Printf("length = %d, err = %+v", i, err)
-	o, _, err := inst.CallString(false, "concat", ", it's a pleasure to meet you!")
-	log.Printf("toString = %v, err = %+v", o, err)
+	log.Printf("Got instance: %+v", inst)
+	err = inst.CallVoid(true, "main", []string{})
+ 	if err != nil {
+		log.Fatalf("Couldn't call main: %v", err)
+	}
 	
 	//strt, _ := gojvm.TypeOf("")
 	//ctx.FindMethod(inst, "toString", strt)
